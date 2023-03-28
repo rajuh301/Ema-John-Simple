8cc170c1566e91cd1cbd8b5bd5946cc68f45b707
rajuh301 <rajuh301@gmail.com>
2023-03-28 14:46:22 +0600

    Show data from card

--- src/componests/Card/Card.jsx
+++ src/componests/Card/Card.jsx
@@ -11,25 +11,27 @@ const Card = (props) => {
 
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
 
-    for(const price of card){
-        total = total + price.price
-        totalShipping = totalShipping + price.shipping;
+    for(const product of card){
+        product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity
+        totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
+
     }
 
     let taxPrice = total * 2 /100;
 
     let grandTotal = total + totalShipping + taxPrice;
 
-
-
-
+    
 
     return (
 
         <div className='card'>
             <h4  style={{fontSize: '21px'}}>Order summary</h4>
-            <p>Select items : {card.length}</p>
+            <p>Select items : {quantity}</p>
             <p>Total Price : $ {total}</p>
             <p>Total Shipping Charge: $ { totalShipping}</p>
             <p>Tax: $ { taxPrice.toFixed(2)}</p>
